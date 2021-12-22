a07c089bbf73f8b83cb4b4e1f8efc22de88daf53
Tim Etchells <tetchel@gmail.com>
2021-12-22 11:03:07 -0800

    update from desktop
    Signed-off-by: Tim Etchells <tetchel@gmail.com>

---
+++ bin/screen-thing.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output HDMI-1 --off --output DP-1 --primary --mode 2560x1440 --pos 1440x0 --rotate normal --output HDMI-2 --off --output HDMI-3 --mode 2560x1440 --pos 0x0 --rotate right
